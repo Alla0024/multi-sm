a9b26724efdf1efb2149809cf5487cc5f4497a35
andrewww05 <118112331+andrewww05@users.noreply.github.com>
2025-07-24 16:51:16 +0300

    article authors fixes

---
+++ resources/lang\uk\article_authors.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'Article_author' => 'article_authors',
+    'title_id' => 'title_id',
+    'search_id' => 'search_id',
+    'title_avatar' => 'title_avatar',
+    'search_avatar' => 'search_avatar',
+    'title_date_of_birth' => 'title_date_of_birth',
+    'search_date_of_birth' => 'search_date_of_birth',
+    'title_facebook' => 'title_facebook',
+    'search_facebook' => 'search_facebook',
+    'title_instagram' => 'title_instagram',
+    'search_instagram' => 'search_instagram',
+    'tab_main' => 'tab_main',
+    'tab_sort' => 'tab_sort',
+];
--- resources/views/pages/article_authors/create.blade.php
+++ resources/views/pages/article_authors/create.blade.php
@@ -6,7 +6,7 @@
             <div class="row mb-2">
                 <div class="col-sm-12">
                     <h1>
-                            {{ $word['create'] }} {!!  $word['Article Author'] !!}
+                            {{ $word['create'] }} {!!  $word['Article_author'] !!}
                     </h1>
                 </div>
             </div>
