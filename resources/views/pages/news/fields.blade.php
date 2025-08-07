7e9eab58bbb7bfb2e99df98c3576fa7f3ff88e98
Dmytro Choban <chobandima2001@gmail.com>
2025-08-07 17:30:59 +0300

    f

--- resources/views/pages/news/fields.blade.php
+++ resources/views/pages/news/fields.blade.php
@@ -161,7 +161,7 @@
         @foreach($languages as $language)
             <div class="input-group mt-3">
                 <span class="input-group-text" id="basic-addon1">{!! $word[$language->id] !!}</span>
-                {!! Form::textarea("descriptions[$language->id][products_title]", null, ['class' => '', 'rows' => 2, 'required']) !!}
+                {!! Form::text("descriptions[$language->id][products_title]", null, ['class' => '', 'required']) !!}
             </div>
         @endforeach
     </div>
