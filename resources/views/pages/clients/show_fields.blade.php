cd7da33b78c7996795d57db2a5d3fa7f9dbecffe
Alla0024 <alla.cheviuk@gmail.com>
2025-09-29 11:14:04 +0300

    generate clients

---
+++ resources/views/pages/clients/show_fields.blade.php
@@ -0,0 +1,154 @@
+<!-- Activation Field -->
+<div class="col-sm-12">
+    {!! Form::label('activation', $word['title_activation']) !!}
+    <p>{{ $client->activation }}</p>
+</div>
+
+
+<!-- Photo Field -->
+<div class="col-sm-12">
+    {!! Form::label('photo', $word['title_photo']) !!}
+    <p>{{ $client->photo }}</p>
+</div>
+
+
+<!-- Surname Field -->
+<div class="col-sm-12">
+    {!! Form::label('surname', $word['title_surname']) !!}
+    <p>{{ $client->surname }}</p>
+</div>
+
+
+<!-- Name Field -->
+<div class="col-sm-12">
+    {!! Form::label('name', $word['title_name']) !!}
+    <p>{{ $client->name }}</p>
+</div>
+
+
+<!-- Birthday Field -->
+<div class="col-sm-12">
+    {!! Form::label('birthday', $word['title_birthday']) !!}
+    <p>{{ $client->birthday }}</p>
+</div>
+
+
+<!-- Email Field -->
+<div class="col-sm-12">
+    {!! Form::label('email', $word['title_email']) !!}
+    <p>{{ $client->email }}</p>
+</div>
+
+
+<!-- Phone Field -->
+<div class="col-sm-12">
+    {!! Form::label('phone', $word['title_phone']) !!}
+    <p>{{ $client->phone }}</p>
+</div>
+
+
+<!-- Password Field -->
+<div class="col-sm-12">
+    {!! Form::label('password', $word['title_password']) !!}
+    <p>{{ $client->password }}</p>
+</div>
+
+
+<!-- User Agent Field -->
+<div class="col-sm-12">
+    {!! Form::label('user_agent', $word['title_user_agent']) !!}
+    <p>{{ $client->user_agent }}</p>
+</div>
+
+
+<!-- Location Id Field -->
+<div class="col-sm-12">
+    {!! Form::label('location_id', $word['title_location_id']) !!}
+    <p>{{ $client->location_id }}</p>
+</div>
+
+
+<!-- Address Field -->
+<div class="col-sm-12">
+    {!! Form::label('address', $word['title_address']) !!}
+    <p>{{ $client->address }}</p>
+</div>
+
+
+<!-- Remember Token Field -->
+<div class="col-sm-12">
+    {!! Form::label('remember_token', $word['title_remember_token']) !!}
+    <p>{{ $client->remember_token }}</p>
+</div>
+
+
+<!-- Temporary Code Field -->
+<div class="col-sm-12">
+    {!! Form::label('temporary_code', $word['title_temporary_code']) !!}
+    <p>{{ $client->temporary_code }}</p>
+</div>
+
+
+<!-- Hash Field -->
+<div class="col-sm-12">
+    {!! Form::label('hash', $word['title_hash']) !!}
+    <p>{{ $client->hash }}</p>
+</div>
+
+
+<!-- Viber Id Field -->
+<div class="col-sm-12">
+    {!! Form::label('viber_id', $word['title_viber_id']) !!}
+    <p>{{ $client->viber_id }}</p>
+</div>
+
+
+<!-- App Token Field -->
+<div class="col-sm-12">
+    {!! Form::label('app_token', $word['title_app_token']) !!}
+    <p>{{ $client->app_token }}</p>
+</div>
+
+
+<!-- Verification Phone Code Field -->
+<div class="col-sm-12">
+    {!! Form::label('verification_phone_code', $word['title_verification_phone_code']) !!}
+    <p>{{ $client->verification_phone_code }}</p>
+</div>
+
+
+<!-- Verification Email Code Field -->
+<div class="col-sm-12">
+    {!! Form::label('verification_email_code', $word['title_verification_email_code']) !!}
+    <p>{{ $client->verification_email_code }}</p>
+</div>
+
+
+<!-- Verified Phone Field -->
+<div class="col-sm-12">
+    {!! Form::label('verified_phone', $word['title_verified_phone']) !!}
+    <p>{{ $client->verified_phone }}</p>
+</div>
+
+
+<!-- Verified Email Field -->
+<div class="col-sm-12">
+    {!! Form::label('verified_email', $word['title_verified_email']) !!}
+    <p>{{ $client->verified_email }}</p>
+</div>
+
+
+<!-- Up To 1C Field -->
+<div class="col-sm-12">
+    {!! Form::label('up_to_1c', $word['title_up_to_1c']) !!}
+    <p>{{ $client->up_to_1c }}</p>
+</div>
+
+
+<!-- Updated At 1C Field -->
+<div class="col-sm-12">
+    {!! Form::label('updated_at_1c', $word['title_updated_at_1c']) !!}
+    <p>{{ $client->updated_at_1c }}</p>
+</div>
+
+
---
+++ resources/views/pages/clients/table.blade.php
@@ -0,0 +1,74 @@
+<div class="card-body p-0">
+
+    <div class="table-responsive">
+        <table class="table" id="clients-table">
+            <thead>
+            <tr>
+                @include('components.basic.search')
+            </tr>
+            <tr>
+                @if(isset($fields))
+                    <th></th>
+                    @foreach($fields as $index => $field)
+                         @if($index != 'id' && $field['inTable'])
+                            <th>{{ $word['title_'.$index] }}</th>
+                        @endif
+                    @endforeach
+                @endif
+                <th colspan="3">{{ $word['action'] }}</th>
+            </tr>
+            </thead>
+            <tbody>
+            @foreach($clients as $client)
+                <tr>
+                    <th>
+                        <div class="input-block input-toggle flex">
+                            <div class="form-check form-switch">
+                                <input class="form-check-input checkbox-child" data-content="{{  $client['id']}}" name="input-toggle_{{ $client['id']}}" type="checkbox" role="switch" id="switchCheckChecked_{{ $client['id']}}">
+                            </div>
+                        </div>
+                    </th>
+
+                    @foreach($fields as $index => $field)
+                        @if($index == 'status' && $field['inTable'])
+                            @if($client[$index] == 1)
+                                <td><div class="status_active">{{ $word['status_1'] }}</div></td>
+                            @else
+                                <td><div class="status_enable">{{ $word['status_0'] }}</div></td>
+                            @endif
+                         @elseif($index == 'image' && $field['inTable'])
+                            <td><img style="width: 140px;" src="{{isset(${{ $config->modelNames->camel }}[$index]) && $client[$index] != '' ? 'https://i.svit-matrasiv.com.ua/images/'.$client[$index] : '/images/common/no_images.png'}}" alt=""></td>
+                         @else
+                             @if($index != 'id' && $field['inTable'])
+                                <td>{{ $client[$index] }}</td>
+                             @endif
+                        @endif
+                    @endforeach
+
+                    <td  colspan="3">
+                        {!! Form::open(['route' => ['clients.destroy', $client->id], 'method' => 'delete']) !!}
+                        <div class='btn-group'>
+
+
+
+
+                            <a href="{{ route('clients.edit', [$client->id]) }}"
+                               class='btn btn-default butt-edit btn-xs'>
+                                <i class="bi bi-pencil fs-40"></i>
+                            </a>
+
+                        </div>
+                        {!! Form::close() !!}
+                    </td>
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
+    </div>
+
+    <div class="card-footer clearfix">
+        <div class="float-right">
+            @include('adminlte-templates::common.paginate', ['records' => $clients])
+        </div>
+    </div>
+</div>
