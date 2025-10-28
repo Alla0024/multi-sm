10f2b37891bcd6c9b38dd9e84cd433a5065de5ba
Alla0024 <alla.cheviuk@gmail.com>
2025-10-28 15:53:42 +0200

    fix

--- database/migrations/2025_10_28_135200_update_orders_table.php
+++ database/migrations/2025_10_28_135200_update_orders_table.php
@@ -4,14 +4,15 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::table('notifications', function (Blueprint $table) {
-            $table->unsignedBigInteger('store_id')->nullable()->after('notification_user_id');
+        Schema::table('orders', function (Blueprint $table) {
+            $table->unsignedBigInteger('store_id')->nullable()->after('order_user_id');
             $table->foreign('store_id')->references('id')->on('stores')->onDelete('cascade');
         });
     }
@@ -21,7 +22,7 @@ return new class extends Migration {
      */
     public function down(): void
     {
-        Schema::table('notifications', function (Blueprint $table) {
+        Schema::table('orders', function (Blueprint $table) {
             $table->dropForeign(['store_id']);
             $table->dropColumn('store_id');
         });
